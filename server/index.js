5ac1627feeba030a6377bc72b6a476f8e446709f
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-09-27 15:12:05 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://blkapi.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://blkapi.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
