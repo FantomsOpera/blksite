83f6821cd634c7b878a311568547462449cf32a9
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-09-27 16:26:37 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 // server/index.js
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const app = express();
-// cors IDIOT
+/* cors IDIOT
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
-});
+});*/
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({ message: "running...", description: "blockarcade cabinet", attributes: [{trait_type:  "main", value: "cabinet"}] });
