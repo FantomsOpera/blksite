2e87a8c93c532155072068bcc904f4cd2cfa18e3
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-09-27 15:18:47 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
 
 // Add headers before the routes are defined
 app.use(cors());
