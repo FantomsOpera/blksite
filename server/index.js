65de43f396c83f307d3899ed060f54c4aa238b65
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-12-24 06:05:20 +1100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -120,7 +120,7 @@ app.get('/fantoms_api', (req, res) => {
 app.get('/feggz_api', (req, res) => {
   const id = req.query.id;
   console.log(id)
-  if (id == "665" || id = 665) {
+  if (id == "665" || id == 665) {
     res.json({ name: "FancyEggz #" + id, image: "https://ipfs.io/ipfs/QmRv1Vveqx4LVdFk1R3wyTtpdau4gkb7BvWRG7825B83qz/-" + id + ".gif", id: id, description: "Fancy Eggz #"+id })
   } else {
     res.json({ name: "FancyEggz #" + id, image: "https://ipfs.io/ipfs/QmRv1Vveqx4LVdFk1R3wyTtpdau4gkb7BvWRG7825B83qz/-" + id + ".jpg", id: id, description: "Fancy Eggz #"+id })
